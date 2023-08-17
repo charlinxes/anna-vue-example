d57930ccd6fb78492782db11721c8244070c2806
charlie <charlinxes@gmail.com>
2023-08-17 17:26:33 +0800

    作業範例：路由練習第二階段部署

---
+++ docs/assets/NotFound-64100664.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index-5c667f6d.js";const n={data(){return{}}},s={style:{padding:"30px"}};function c(r,a,_,p,d,i){return e(),o("section",s,"404 NOT FOUND!")}const u=t(n,[["render",c]]);export{u as default};
