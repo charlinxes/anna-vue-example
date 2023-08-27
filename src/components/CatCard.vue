1434e13d0ef7a41168585e1d7140b17adb258db0
charlie <charlinxes@gmail.com>
2023-08-27 16:46:04 +0800

    作業checkbox

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -1,8 +1,21 @@
 <template>
-    <main class="catCard">
-        <h4 style="margin:0">{{ catData.name }}</h4>
-        <p>年齡：{{ catData.age }}</p>
-        <p>描述：{{ catData.des }}</p>
+    <main class="catCard" :class="{ checked: cardChecked[0] }" style="color: rgb(112, 50, 6);" @click="toggleChecked">
+        <h4 style="margin:0">
+            {{ catData.name }}
+            <input type="checkbox" class="pointer-events:none" :value="catData.name" v-model="cardChecked">
+        </h4>
+        <div style="display:flex;margin: 16px 0 8px">
+            <p style="flex-shrink: 0;width:50px;">年齡：</p>
+            <p>{{ catData.age }}</p>
+        </div>
+        <div style="display:flex;margin: 16px 0 8px">
+            <p style="flex-shrink: 0;width:50px;">描述：</p>
+            <p>{{ catData.des }}</p>
+        </div>
+        <div style="display:flex">
+            <p style="flex-shrink: 0;width:50px;">住址：</p>
+            <p>{{ catData.address }}</p>
+        </div>
     </main>
 </template>
 
@@ -11,11 +24,18 @@ export default {
     props: ['catData'],
     data() {
         return {
+            cardChecked: []
         }
     },
-    computed: {
-    },
     methods: {
+        toggleChecked() {
+            if (this.cardChecked[0]) {
+                this.cardChecked.splice(0, 1)
+            } else {
+                this.cardChecked.push(this.catData.name)
+            }
+            this.$emit('toggleChecked', this.catData)
+        }
     }
 
 }
@@ -24,9 +44,28 @@ export default {
 <style>
 .catCard {
     width: 350px;
-    height: 200px;
-    border: 1px solid black;
+    border: 1px solid rgb(179, 80, 9);
     padding: 40px;
-    border-radius: 100px
+    border-radius: 100px;
+    transition: all 1s cubic-bezier(0.01, 0.31, 0.74, 0.66);
+    box-shadow: 0 3px 3px rgb(142, 63, 7);
+    background-color: rgb(251, 250, 248)
+}
+
+.catCard.checked {
+    background-color: rgb(243, 233, 220)
+}
+
+.catCard input {
+    accent-color: rgb(142, 63, 7);
+}
+
+.catCard:hover {
+    transform: translateY(-2px);
+    box-shadow: 0 5px 3px rgb(133, 60, 7);
+}
+
+p {
+    margin: 0;
 }
 </style>
--- src/pages/ProductView.vue
+++ src/pages/ProductView.vue
@@ -1,6 +1,15 @@
 <template>
-    <main style="padding:30px;display:flex;flex-wrap:wrap;gap:20px 20px">
-        <CatCard v-for="cat in catList" :catData="cat" />
+    <main style="padding:30px;">
+        <div style="display:flex;flex-wrap:wrap;gap:20px 20px;margin-bottom: 30px;" v-show="!showDataChange">
+            <CatCard v-for="cat in catList" ref="catList" :key="cat.id" :catData="cat" @toggleChecked="setCheckedList" />
+        </div>
+        <button v-show="!showDataChange" @click="showDataChange = true">變更地址</button>
+        <div v-show="showDataChange">
+            <div>新地址：<input v-model="catNewAddress" /></div>
+            <button @click="changeAddress" style="margin-right:10px">送出</button>
+            <button @click="showDataChange = false">取消變更</button>
+        </div>
+
     </main>
 </template>
 
@@ -26,15 +35,43 @@ export default {
     data() {
         return {
             catList: [
-                { name: '大白', birth: '2020/10/11', des: '我是胖胖大白貓，我欺軟怕硬，最愛欺負小貓' },
-                { name: '紅紅', birth: '2019/4/9', des: '請叫我瘦弱紅紅，貓中一隻花，曾經也是众星拱月，直到我被絕了育QQ' },
-                { name: '黑仔', birth: '2022/10/29', des: '酷酷黑仔就是我，別隨便摸我，誰惹我我跟誰急' },
-                { name: '小黃', birth: '2023/1/23', des: '我明明毛色不黃，但不知道為什麼大家叫我小黃，可能我比較性感吧:D。大白我還在講話你不要弄我' },
-                { name: '阿花', birth: '2021/11/14', des: '我不喜歡別人盯著我看，因為晚上都睡不好，臉很花長了一堆痘痘' }
-            ]
+                { id: 1, name: '大白', birth: '2020/10/11', des: '我是胖胖大白貓，我欺軟怕硬，最愛欺負小貓', address: 'A' },
+                { id: 2, name: '紅紅', birth: '2019/4/9', des: '請叫我瘦弱紅紅，貓中一隻花，曾經也是众星拱月，直到我被絕了育QQ', address: 'B' },
+                { id: 3, name: '黑仔', birth: '2022/10/29', des: '酷酷黑仔就是我，別隨便摸我，誰惹我我跟誰急', address: 'C' },
+                { id: 4, name: '小黃', birth: '2023/1/23', des: '我明明毛色不黃，但不知道為什麼大家叫我小黃，可能我比較性感吧:D。大白我還在講話你不要弄我', address: 'D' },
+                { id: 5, name: '阿花', birth: '2021/11/14', des: '我不喜歡別人盯著我看，因為晚上都睡不好，臉很花長了一堆痘痘', address: 'E' }
+            ],
+            checkedList: [],
+            catNewAddress: '',
+            showDataChange: false
         }
     },
     methods: {
+        setCheckedList(catObj) {
+            const targetIndex = this.checkedList.findIndex(item => item.id === catObj.id)
+            if (targetIndex >= 0) {
+                console.log(targetIndex)
+                this.checkedList.splice(targetIndex, 1)
+            } else {
+                this.checkedList.push(catObj)
+            }
+        },
+        changeAddress() {
+            this.catList = this.catList.map(cur => {
+                console.log(this.checkedList)
+                if (this.checkedList.some(item => item.id === cur.id)) {
+                    return { ...cur, address: this.catNewAddress }
+                }
+                return cur
+            })
+            this.checkedList.forEach(cur => {
+                this.$refs.catList.forEach((item) => {
+                    item.cardChecked = []
+                })
+            })
+            this.checkedList = []
+            this.showDataChange = false
+        }
     }
 
 }
