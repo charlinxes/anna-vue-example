0ad8b0a2aa311f79a54f3f9ce22669ec5fd60dc5
charlie <charlinxes@gmail.com>
2023-10-17 11:59:37 +0800

    10/17 作業

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,51 +1,114 @@
 <template>
-    <div style="padding: 30px;">
-        <el-button type="success" @click="addCard">添加卡片</el-button>
+    <div style="padding: 30px;display: flex;align-items: center;">
+        <el-button type="success" @click="addNewCard">添加卡片</el-button>
         <el-button type="danger" @click="sortCard">更改排序</el-button>
-        <span v-if="sortStatus === 'descend'" style="margin-left: 5px">升序<el-icon style="position: relative;top:2px">
-                <SortUp />
-            </el-icon></span>
-        <span v-if="sortStatus === 'ascend'" style="margin-left: 5px">降序<el-icon style="position: relative;top:2px">
-                <SortDown />
-            </el-icon></span>
-        <TransitionGroup name="list">
-            <TodoCard v-for="card in cardList" :key="card.time" :cardData="card" />
-        </TransitionGroup>
+        <span style="margin-left: 10px">{{ sortOrder }}</span>
+        <el-icon v-if="sortOrder === '升序'">
+            <SortUp />
+        </el-icon>
+        <el-icon v-else="sortOrder === '降序'">
+            <SortDown />
+        </el-icon>
+        <el-select v-model="filterTag" style="margin-left: 10px" placeholder="過濾項目">
+            <el-option v-for="item in filterOptions" :key="item.value" :label="item.label" :value="item.value" />
+        </el-select>
     </div>
+    <el-row style="padding: 30px;" :gutter="16">
+        <TransitionGroup name="list">
+            <el-col v-for="(card, idx) in filterCardList" :key="card" :span="8">
+                <el-card :body-style="{ padding: '0px' }">
+                    <div style="padding: 14px">
+                        <span>我是第{{ card.id }}個</span><el-tag style="margin-left: 10px;" :type="card.id % 2 ===
+                            0 ? 'success' : 'warning'">{{ card.id % 2 ===
+        0 ? '偶數' : '奇數' }}</el-tag>
+                        <div class="bottom">
+                            生成時間：
+                            <time class="time">{{ card.currentTime.toLocaleString('zh-TW') }}</time>
+                        </div>
+                        <div style="text-align: end">
+                            <el-button type="danger" :icon="Delete" circle @click="delCard(card.id)" />
+                        </div>
+
+                    </div>
+                </el-card>
+            </el-col>
+        </TransitionGroup>
+    </el-row>
 </template>
 
 <script setup>
-import TodoCard from '../components/TodoCard.vue'
-import { ref } from 'vue'
-
+import { ref, computed } from 'vue'
+import { Delete } from '@element-plus/icons-vue'
 const cardList = ref([])
-let index = 0
-let sortStatus = ref('descend')
+const sortOrder = ref('升序')
+const filterTag = ref('')
+const filterOptions = ref([
+    {
+        value: '',
+        label: '無'
+    },
+    {
+        value: 'odd',
+        label: '奇數'
+    },
+    {
+        value: 'even',
+        label: '偶數'
+    }
+])
+let idx = 0
 
-function addCard() {
-    cardList.value.push({ display: `我是第${index}個`, time: Date.now() })
-    index += 1
-    console.log(cardList.value)
+function addNewCard() {
+    cardList.value.push({ id: idx, currentTime: new Date() })
+    idx += 1
+}
+
+function delCard(id) {
+    const idx = cardList.value.findIndex((item) => {
+        return item.id === id
+    })
+    cardList.value.splice(idx, 1)
 }
 
 function sortCard() {
-    if (sortStatus.value !== 'descend') {
+    if (sortOrder.value === '升序') {
         cardList.value.sort((a, b) => {
-            return a.time - b.time
+            return b.currentTime - a.currentTime
         })
-        sortStatus.value = 'descend'
+        sortOrder.value = '降序'
     } else {
         cardList.value.sort((a, b) => {
-            return b.time - a.time
+            return a.currentTime - b.currentTime
         })
-        sortStatus.value = 'ascend'
+        sortOrder.value = '升序'
     }
 }
+
+const filterCardList = computed(() => {
+    switch (filterTag.value) {
+        case '':
+            return cardList.value
+        case 'even':
+            return cardList.value.filter((item) => {
+                return item.id % 2 === 0
+            })
+        case 'odd':
+            return cardList.value.filter((item) => {
+                return item.id % 2 !== 0
+            })
+    }
+})
+
+
 </script>
 
 <style lang="scss" scoped>
+.time {
+    display: block;
+    font-size: 12px;
+}
+
 .list-move,
-/* 对移动中的元素应用的过渡 */
 .list-enter-active,
 .list-leave-active {
     transition: all 0.5s ease;
@@ -57,9 +120,11 @@ function sortCard() {
     transform: translateX(30px);
 }
 
-/* 确保将离开的元素从布局流中删除
-  以便能够正确地计算移动的动画。 */
 .list-leave-active {
     position: absolute;
+}
+
+::v-deep .el-col {
+    margin-bottom: 16px;
 }
 </style>
